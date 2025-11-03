c0f7547f559a103968ab791033ee8a23334ee08a
James Kay <james.kay@linera.io>
2025-11-03 17:31:35 +0000

    raise thread termination as an error

--- src/worker.js
+++ src/worker.js
@@ -4,20 +4,27 @@
 // know where it is yet.
 import * as wasm from 'web-thread:wasm-shim';
 
+const pending = new Map();
+
 self.onmessage = async (event) => {
   if (event.data.type === 'init') {
     await wasm.default(event.data);
     self.postMessage({ type: 'ready' });
   } else if (event.data.type === 'destroy') {
+    for (const id of pending.keys())
+      self.postMessage({ type: 'response', id, error: new Error('thread destroyed during execution') });
     self.close();
   } else if (event.data.type === 'run') {
     const { id, code, context } = event.data;
+    pending.set(id, null);
     try {
       const { message: result, transfer } = await wasm.__web_thread_worker_entry_point(code, context);
       self.postMessage({ type: 'response', id, result }, transfer);
     } catch (error) {
       console.error(error);
       self.postMessage({ type: 'response', id, error });
+    } finally {
+      pending.delete(id);
     }
   } else {
     console.error('[web-thread] malformed request', event.data);
