c058ffc30c87ed15579d4d314395a0401a7c19c2
James Kay <james.kay@linera.io>
2025-11-13 12:47:30 +0000

    `web-thread-select`: use the shim if we are not compiling for `wasm32`

--- select/src/lib.rs
+++ select/src/lib.rs
@@ -1,4 +1,19 @@
-#[cfg(feature = "web")]
-pub use web_thread::*;
-#[cfg(not(feature = "web"))]
-pub use web_thread_shim::*;
+/*!
+
+# `web-thread-select`
+
+This crate allows selecting between `web-thread` and `web-thread-shim`
+using a feature flag.
+
+If the target is a `wasm32` architecture and the `web` feature flag is
+passed, we use the Web implementation of `web-thread`; otherwise, we
+transparently substitute in the shim.
+*/
+
+cfg_if::cfg_if! {
+    if #[cfg(all(target_arch = "wasm32", feature = "web"))] {
+        pub use web_thread::*;
+    } else {
+        pub use web_thread_shim::*;
+    }
+}
